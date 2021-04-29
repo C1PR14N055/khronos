070de15cd8c1d5be343a946e797e284a621de661
ciprian <ciprianosss@gmail.com>
2021-04-29 17:38:26 +0300

    updates to UI

--- src/main/clockui.cpp
+++ src/main/clockui.cpp
@@ -7,10 +7,12 @@ void clockUI::render()
     char cTime[128];
     sprintf(cTime, "TIME: %02d:%02d:%02d ", gps->time.hour(), gps->time.minute(), gps->time.second());
     lcd::print(0, 0, cTime);
+    Serial.println(cTime);
 
     char cDate[128];
     sprintf(cDate, "DATE: %02d/%02d/%02d ", gps->date.day(), gps->date.month(), gps->date.year());
     lcd::print(0, 1, cDate);
+    Serial.println(cDate);
 }
 
 void clockUI::onLoop()
--- src/main/main.ino
+++ src/main/main.ino
@@ -8,6 +8,7 @@ enum FUNCTION_MODES
 {
     CLOCK = 0,
     GPS = 1,
+    SWITCHING = 2,
 };
 
 // default mode (clock)
@@ -18,14 +19,55 @@ const int REED_PIN = A0;
 const int LED_R_PIN = A1;
 const int LED_G_PIN = A2;
 const int LED_B_PIN = A3;
+// ADC VCC BAT for Vumeter
+const int ADC_VCC_BAT = A4;
 
 // The TinyGPS++ object
 gpsUI gpsui;
 clockUI clockui;
 TinyGPSPlus gps;
 
+float getBatteryVoltage()
+{
+    // 5V max = 1023, divided by precision 1024 (10 bit ADC)
+    float v_bat = analogRead(ADC_VCC_BAT);
+    v_bat = 5 * v_bat / 1024;
+    Serial.print("VCC BAT: ");
+    Serial.print(v_bat);
+    Serial.println("V");
+
+    return v_bat;
+}
+
+void lightLED(bool light)
+{
+    digitalWrite(LED_BUILTIN, light ? HIGH : LOW);
+}
+
+// R - lred G - green B - blue, start = start millis(), time = total todo
+void lightRGB(bool R, bool G, bool B)
+{
+    // reset
+    lightRGB(false, false, false);
+
+    digitalWrite(LED_R_PIN, R ? HIGH : LOW);
+    digitalWrite(LED_G_PIN, G ? HIGH : LOW);
+    digitalWrite(LED_B_PIN, B ? HIGH : LOW);
+}
+
+// main.ino render access to LCD
+void postRender()
+{
+    float v_bat_percent = getBatteryVoltage() * 100 / 5;
+    // Battery level
+    lcd::printBatteryIcon(15, 0, v_bat_percent);
+}
+
 void setup()
 {
+    // init magnetic switch pin as input
+    pinMode(REED_PIN, INPUT_PULLUP);
+
     // init digital pin LED_BUILTIN as an output
     pinMode(LED_BUILTIN, OUTPUT);
 
@@ -34,11 +76,14 @@ void setup()
     pinMode(LED_G_PIN, OUTPUT);
     pinMode(LED_B_PIN, OUTPUT);
 
-    // init magnetic switch pin as input
-    pinMode(REED_PIN, INPUT_PULLUP);
+    // init ADC
+    pinMode(ADC_VCC_BAT, INPUT);
 
     // init LCD
     lcd::init();
+
+    // lcd clear screen
+    lcd::clear();
 
     // Set GPS instance available to gpsui and clockui
     gpsui.setGps(&gps);
@@ -50,32 +95,9 @@ void setup()
     clockui.setEnabled(true);
 }
 
-// TODO: separate RGB
-void blinkDefaultLED()
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(15);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(15);
-
-    digitalWrite(LED_R_PIN, HIGH);
-    delay(17);
-    digitalWrite(LED_R_PIN, LOW);
-    delay(17);
-
-    digitalWrite(LED_G_PIN, HIGH);
-    delay(17);
-    digitalWrite(LED_G_PIN, LOW);
-    delay(17);
-
-    digitalWrite(LED_B_PIN, HIGH);
-    delay(17);
-    digitalWrite(LED_B_PIN, LOW);
-    delay(17);
-}
-
 void loop()
 {
+    FUNCTION_MODES prevMode = mode;
     // Read the state of the switch
     int proximity = digitalRead(REED_PIN);
     // switch timer
@@ -83,29 +105,40 @@ void loop()
     // If the pin reads low for 5 seconds, the switch is closed
     while (proximity == LOW && millis() - switchStart <= 5000)
     {
-        // waiting for 5 seconds
+        // waiting for 5 seconds while magnet still close
         proximity = digitalRead(REED_PIN);
-        // blink delays
-        blinkDefaultLED();
+        // blink RGB
+        lightRGB(false, false, millis() % 100);
+        // blink LED
+        lightLED(millis() % 100);
     }
 
     // If the timer passed the 5 second mark
     if (millis() - switchStart >= 5000)
     {
+        // lcd clear screen
+        lcd::clear();
         // toggle modes
-        if (mode == FUNCTION_MODES::CLOCK)
+        switch (prevMode)
         {
+        case FUNCTION_MODES::SWITCHING:
+            // nothing
+            break;
+        case FUNCTION_MODES::CLOCK:
             mode = FUNCTION_MODES::GPS;
             clockui.setEnabled(false);
             gpsui.setEnabled(true);
             Serial.print("Switch closed, GPS ENABLED");
-        }
-        else
-        {
+            break;
+        case FUNCTION_MODES::GPS:
             mode = FUNCTION_MODES::CLOCK;
             gpsui.setEnabled(false);
             clockui.setEnabled(true);
             Serial.print("Switch opened, CLOCK ENABLED");
+            break;
+        default:
+            mode = FUNCTION_MODES::CLOCK;
+            break;
         }
     }
 
@@ -115,8 +148,22 @@ void loop()
     {
         while (Serial1.available())
             gps.encode(Serial1.read());
-    } while (millis() - gpsStart < 1000 / 30);
+    } while (millis() - gpsStart < 1000 / 10);
 
+    // gps main loop
     gpsui.onLoop();
+    // clock main loop
     clockui.onLoop();
+
+    // post-render => battery info
+    postRender();
+    // show RGB LED status
+    if (mode == FUNCTION_MODES::CLOCK)
+    {
+        lightRGB(true, false, false);
+    }
+    if (mode == FUNCTION_MODES::GPS)
+    {
+        lightRGB(true, true, false);
+    }
 }
